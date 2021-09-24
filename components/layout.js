5d2fc299ee2c68bd5df9a54fa8002a9b894a6a77
Philipp Overfeld <Overfeld.Philipp@gmail.com>
2021-09-24 17:11:46 +0200

    update

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Integration Test'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'NextJS Integration Test'
 
 export default function Layout({ children, home }) {
   return (
